2e0a819e0d7769d2722eeba4b4c699d53a94a484
Thomas Huber <thuber3@dxc.com>
2026-02-21 12:27:51 +0100

    Fixed build warning about chunk size > 500kB

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,25 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => ({
   base: command === "build" ? "/client/" : "/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules/rete") || id.includes("node_modules/rete-")) {
+            return "rete-vendor";
+          }
+
+          if (
+            id.includes("node_modules/react") ||
+            id.includes("node_modules/react-dom") ||
+            id.includes("node_modules/scheduler")
+          ) {
+            return "react-vendor";
+          }
+        }
+      }
+    }
+  },
   server: {
     host: "0.0.0.0",
     port: 5173
